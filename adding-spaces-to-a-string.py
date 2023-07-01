703d431e6e5d5a2add48ddba9778cc4c9509a88b
Gashahun <130774521+gashu2@users.noreply.github.com>
2023-07-01 22:58:45 +0300

    Update adding-spaces-to-a-string.py

--- adding-spaces-to-a-string.py
+++ adding-spaces-to-a-string.py
@@ -1,10 +1,11 @@
 class Solution:
     def addSpaces(self, s: str, spaces: list[int]) -> str:
-        stmt_list = ''
-        for i in range(len(s)):
-            if i in spaces:
-                stmt_list += ' '
-            stmt_list += s[i]
-        return stmt_list
+        stmt_list = []
+        prev = 0
+        for space in spaces:
+            stmt_list.append(s[prev:space] + ' ')
+            prev = space
+        stmt_list.append(s[prev:])
+        return ''.join(stmt_list)
 sol1 = Solution()
-print(sol1.addSpaces("LeetcodeHelpsMeLearn", [8,13,15]))
+print(sol1.addSpaces("spacing", [0, 1, 2, 3, 4, 5, 6]))

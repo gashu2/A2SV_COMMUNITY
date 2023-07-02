a94ef52f163f91e67633cb05430942493f108289
Gashahun <130774521+gashu2@users.noreply.github.com>
2023-07-02 20:00:23 +0300

    Update valid-anagram.py

--- valid-anagram.py
+++ valid-anagram.py
@@ -1,19 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        li_one = []
-        li_two = []
-        for i in s:
-            li_one.append(i)
-        for j in t:
-            li_two.append(j)
-        li_one.sort()
-        li_two.sort()
-        return(li_one == li_two)
-        
-
-
-
-
-            
-
-
+        return sorted(s) == sorted(t)
